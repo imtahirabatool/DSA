1a61dd6696638a31855c8e5ab9f8235e615e039d
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-05-07 13:06:39 +0500

    Update 2-D Array.cpp

--- PRE/Arrays/2-D Array.cpp
+++ PRE/Arrays/2-D Array.cpp
@@ -3,25 +3,30 @@
 using namespace std;
 
 int main(){
-	int n,m;
-	cout<<"Enter number of rows:";
-	cin>>n;
-	cout<<"Enter number of columns:";
-	cin>>m;
-	int arr[n][m];
-	int *p=arr[0];
-	cout<<"Enter elements of Array:"<<endl;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cin>>arr[i][j];
-		}
-	}
-	for(int i=0;i<n;i++){
-		for(int j=0;j<m;j++){
-			cout<<arr[i][j]<<"  "<<endl;
-		}
-	}
-	cout<<"Address:"<<p;
-	
-	return arr[n][m];
+    int n, m;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    cout << "Enter number of columns: ";
+    cin >> m;
+    int arr[n][m];
+    int *p = &arr[0][0]; // Getting the address of the first element of the array
+
+    cout << "Enter elements of Array:" << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Elements of Array:" << endl;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout << arr[i][j] << "  "; // No extra newline after each element
+        }
+        cout << endl; // Add newline after each row
+    }
+
+    cout << "Address: " << p << endl; // Print the address of the first element
+
+    return 0; // Return 0 to indicate successful execution
 }
